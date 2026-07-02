8221814bf4a23ab33187f128ecb5cc9af737b3c1
Ali Yahya <ali.yahya.softwareenginee@gmail.com>
2026-07-02 18:24:39 +0300

    Update DBcreator to work correctly

--- DBcreator.sql
+++ DBcreator.sql
@@ -1,48 +1,46 @@
-USE [ContactsDB]
+USE [ContactsDB];
 GO
 
-/****** Object:  Table [dbo].[Contacts]    Script Date: 6/25/2026 8:22:46 PM ******/
-SET ANSI_NULLS ON
+SET ANSI_NULLS ON;
 GO
 
-SET QUOTED_IDENTIFIER ON
+SET QUOTED_IDENTIFIER ON;
 GO
 
-CREATE TABLE [dbo].[Contacts](
-	[ContactID] [int] IDENTITY(1,1) NOT NULL,
-	[FirstName] [nvarchar](50) NOT NULL,
-	[LastName] [nvarchar](50) NOT NULL,
-	[Email] [nvarchar](50) NOT NULL,
-	[Phone] [nvarchar](20) NOT NULL,
-	[Address] [nvarchar](200) NOT NULL,
-	[DateOfBirth] [datetime] NOT NULL,
-	[CountryID] [int] NOT NULL,
-	[ImagePath] [nvarchar](500) NULL,
- CONSTRAINT [PK__Contacts__5C6625BBE2F19656] PRIMARY KEY CLUSTERED 
+-- Create Countries table first
+CREATE TABLE [dbo].[Countries]
 (
-	[ContactID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+    [CountryID] INT IDENTITY(1,1) NOT NULL,
+    [CountryName] NVARCHAR(50) NULL,
+    [Code] NVARCHAR(3) NULL,
+    [PhoneCode] NVARCHAR(3) NULL,
+
+    CONSTRAINT [PK_Countries]
+        PRIMARY KEY CLUSTERED ([CountryID] ASC)
+);
 GO
 
-ALTER TABLE [dbo].[Contacts]  WITH CHECK ADD  CONSTRAINT [FK__Contacts__Countr__398D8EEE] FOREIGN KEY([CountryID])
-REFERENCES [dbo].[Countries] ([CountryID])
-GO
-
-ALTER TABLE [dbo].[Contacts] CHECK CONSTRAINT [FK__Contacts__Countr__398D8EEE]
-GO
-
-
-CREATE TABLE [dbo].[Countries](
-	[CountryID] [int] IDENTITY(1,1) NOT NULL,
-	[CountryName] [nvarchar](50) NULL,
-	[Code] [nvarchar](3) NULL,
-	[PhoneCode] [nvarchar](3) NULL,
-PRIMARY KEY CLUSTERED 
+-- Create Contacts table
+CREATE TABLE [dbo].[Contacts]
 (
-	[CountryID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+    [ContactID] INT IDENTITY(1,1) NOT NULL,
+    [FirstName] NVARCHAR(50) NOT NULL,
+    [LastName] NVARCHAR(50) NOT NULL,
+    [Email] NVARCHAR(50) NOT NULL,
+    [Phone] NVARCHAR(20) NOT NULL,
+    [Address] NVARCHAR(200) NOT NULL,
+    [DateOfBirth] DATETIME NOT NULL,
+    [CountryID] INT NOT NULL,
+    [ImagePath] NVARCHAR(500) NULL,
+
+    CONSTRAINT [PK_Contacts]
+        PRIMARY KEY CLUSTERED ([ContactID] ASC)
+);
 GO
 
-
+-- Create Foreign Key
+ALTER TABLE [dbo].[Contacts]
+ADD CONSTRAINT [FK_Contacts_Countries]
+FOREIGN KEY ([CountryID])
+REFERENCES [dbo].[Countries] ([CountryID]);
+GO
